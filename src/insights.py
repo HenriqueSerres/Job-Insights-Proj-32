a615e9639b673fac18801d33fcfef04fbe80c25c
HenriqueSerres <henriqueserres@gmail.com>
2022-09-12 19:52:22 -0300

    add: req 5

--- src/insights.py
+++ src/insights.py
@@ -69,21 +69,13 @@ def get_max_salary(path):
 
 
 def get_min_salary(path):
-    """Get the minimum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
-    pass
+    jobs_data = read(path)
+    min_salary = []
+    for job in jobs_data:
+        if job['min_salary'].isdigit() and job['min_salary'] != '':
+            min_salary.append(int(job['min_salary']))
+    print(min_salary)
+    return min(min_salary)
 
 
 def matches_salary_range(job, salary):
