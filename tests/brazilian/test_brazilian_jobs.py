2ba119d2cac4d03c856c72d5bb35ec03424395dd
HenriqueSerres <henriqueserres@gmail.com>
2022-09-13 19:28:00 -0300

    add: req 11 fixed is now or never

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -3,7 +3,7 @@ from src.brazilian_jobs import read_brazilian_file
 
 def test_brazilian_jobs():
     result = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
-    english_keys = [key for key in result]
-    assert "title" in english_keys
-    assert "salary" in english_keys
-    assert "type" in english_keys
+    for key in result:
+        assert "title" in key
+        assert "salary" in key
+        assert "type" in key
