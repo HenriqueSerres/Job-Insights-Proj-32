c2d821bc7425fd2fb9971d4e8b59a22faede5be8
HenriqueSerres <henriqueserres@gmail.com>
2022-09-13 18:55:29 -0300

    add: req 11 fixed

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -3,7 +3,7 @@ from src.brazilian_jobs import read_brazilian_file
 
 def test_brazilian_jobs():
     result = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
-    assert result == {
+    assert result.keys() == {
         "title": "Maquinista",
         "salary": "2000",
         "type": "trainee",
